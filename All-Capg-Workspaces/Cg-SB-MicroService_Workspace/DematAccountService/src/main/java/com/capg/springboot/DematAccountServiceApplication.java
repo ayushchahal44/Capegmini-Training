b9b6508dcd7787d33b04a5f31cef8924105f07c4
Ayush <ayush.chahal@lpu.in>
2026-03-11 11:28:22 +0530

    setup: create workspace for microservices project with Eureka, Gateway, and services

---
+++ All-Capg-Workspaces/Cg-SB-MicroService_Workspace/DematAccountService/src/main/java/com/capg/springboot/DematAccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.capg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DematAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DematAccountServiceApplication.class, args);
+	}
+
+}
