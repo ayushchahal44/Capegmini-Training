f84fdc95e4e0168f6304fc5f597ea635038564e8
Ayush <ayush.chahal@lpu.in>
2026-03-19 11:38:27 +0530

    Adding Docker programs.

---
+++ All-Capg-Workspaces/Workspace-Spring-SpringBoot/Docker01_MovieService/src/main/java/com/capg/springboot/Docker01MovieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker01MovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker01MovieServiceApplication.class, args);
+	}
+
+}
